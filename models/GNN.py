e6a2ff80ee0fbcc902cdb46a9858d579de933469
Conite002 <conite.gbodogbe@imsp-uac.org>
2025-01-19 00:14:14 +0100

    Update GNN model to use correct argument names and add documentation

--- models/GNN.py
+++ models/GNN.py
@@ -9,8 +9,8 @@ class GNN(nn.Module):
         self.num_modals = num_modals
         print(f"GNN -> Use RGCNConv & TransformerConv")
         self.conv1 = RGCNConv(g_dim, h1_dim, num_relations)
-        self.conv2 = TransformerConv(h1_dim, h2_dim, heads=args.graph_transformer_nheads, dropout=args.graph_transformer_dropout, concat=True)
-        self.bn = nn.BatchNorm1d(h2_dim * args.graph_transformer_nheads)
+        self.conv2 = TransformerConv(h1_dim, h2_dim, heads=args.graph_transformers_nheads, dropout=args.graph_transformer_dropout, concat=True)
+        self.bn = nn.BatchNorm1d(h2_dim * args.graph_transformers_nheads)
 
     def forward(self, node_features, node_type, edge_index, edge_type):
         x = self.conv1(node_features, edge_index, edge_type)
